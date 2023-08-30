b16775d65db4334c6019f34aee1cd9fadc7b06b1
jayricka <jayricka17@gmail.com>
2023-08-30 16:31:06 +0300

    debug session

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,58 +1,52 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
- * Return: Pointer to the lowest common ancestor node, or NULL if not found
- */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+* binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+* @first: Pointer to the first node
+* @second: Pointer to the second node
+* Return: Pointer to the lowest common ancestor node, or NULL if not found
+*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
-	const binary_tree_t *ptr1, *ptr2;
-	size_t depth1 = 0, depth2 = 0;
+const binary_tree_t *ptr1 = first, *ptr2 = second;
+size_t depth1 = 0, depth2 = 0;
 
-	if (!first || !second)
-		return (NULL);
+if (!first || !second)
+return (NULL);
 
-	ptr1 = first;
-	ptr2 = second;
+while (ptr1)
+{
+ptr1 = ptr1->parent;
+depth1++;
+}
 
-	/* Calculate the depth of each node */
-	while (ptr1)
-	{
-		ptr1 = ptr1->parent;
-		depth1++;
-	}
+while (ptr2)
+{
+ptr2 = ptr2->parent;
+depth2++;
+}
 
-	while (ptr2)
-	{
-		ptr2 = ptr2->parent;
-		depth2++;
-	}
+ptr1 = first;
+ptr2 = second;
 
-	/* Reset pointers to the original nodes */
-	ptr1 = first;
-	ptr2 = second;
+while (depth1 > depth2)
+{
+ptr1 = ptr1->parent;
+depth1--;
+}
 
-	/* Move the deeper node up to the same depth as the other node */
-	while (depth1 > depth2)
-	{
-		ptr1 = ptr1->parent;
-		depth1--;
-	}
+while (depth2 > depth1)
+{
+ptr2 = ptr2->parent;
+depth2--;
+}
 
-	while (depth2 > depth1)
-	{
-		ptr2 = ptr2->parent;
-		depth2--;
-	}
+while (ptr1 && ptr2 && ptr1 != ptr2)
+{
+ptr1 = ptr1->parent;
+ptr2 = ptr2->parent;
+}
 
-	/* Move both nodes up in tandem until the common ancestor is found */
-	while (ptr1 && ptr2 && ptr1 != ptr2)
-	{
-		ptr1 = ptr1->parent;
-		ptr2 = ptr2->parent;
-	}
-
-	return ((binary_tree_t *)ptr1);
+return ((binary_tree_t *)ptr1);
 }
