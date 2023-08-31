b30bceb85f5e018e5567d164a630aeac88d2b5bf
jayricka <jayricka17@gmail.com>
2023-08-31 11:06:40 +0300

    insert in bst

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,63 +1,46 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * bst_insert - Inserts a value into a Binary Search Tree (BST)
- * @tree: A double pointer to the root node of the BST
- * @value: The value to insert
- *
- * Return: A pointer to the newly created node, or NULL on failure
- */
+* bst_insert - Inserts a value in a Binary Search Tree (BST).
+* @tree: A double pointer to the root node of the BST.
+* @value: The value to insert.
+* Return: A pointer to the created node, or NULL on failure.
+*/
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    bst_t *new_node, *current;
+if (!tree)
+return (NULL);
 
-    if (tree == NULL)
-        return (NULL);
-
-    /* Create a new node */
-    new_node = binary_tree_node(NULL, value);
-
-    if (new_node == NULL)
-        return (NULL);
-
-    /* If the tree is empty, make the new node the root */
-    if (*tree == NULL)
-    {
-        *tree = new_node;
-        return (new_node);
-    }
-
-    /* Traverse the tree to find the correct position to insert */
-    current = *tree;
-    while (current)
-    {
-        if (value == current->n)
-        {
-            free(new_node);
-            return (NULL); /* Value already exists, ignore */
-        }
-        if (value < current->n)
-        {
-            if (current->left == NULL)
-            {
-                new_node->parent = current;
-                current->left = new_node;
-                return (new_node);
-            }
-            current = current->left;
-        }
-        else
-        {
-            if (current->right == NULL)
-            {
-                new_node->parent = current;
-                current->right = new_node;
-                return (new_node);
-            }
-            current = current->right;
-        }
-    }
-
-    return (NULL); /* Should not reach here */
+/* If the tree is empty, create a new node as the root */
+if (*tree == NULL)
+{
+*tree = binary_tree_node(NULL, value);
+return (*tree);
 }
+
+/* Traverse the tree to find the appropriate location for insertion */
+if (value < (*tree)->n)
+{
+if ((*tree)->left == NULL)
+{
+(*tree)->left = binary_tree_node(*tree, value);
+return ((*tree)->left);
+}
+else
+return (bst_insert(&((*tree)->left), value));
+}
+else if (value > (*tree)->n)
+{
+if ((*tree)->right == NULL)
+{
+(*tree)->right = binary_tree_node(*tree, value);
+return ((*tree)->right);
+}
+else
+return (bst_insert(&((*tree)->right), value));
+}
+
+/* If the value is already in the tree, return NULL (no duplicates) */
+return (NULL);
+}
+
