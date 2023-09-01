f0e3b4c14184501ad895a22d37b7978b0ee38576
jayricka <jayricka17@gmail.com>
2023-09-01 05:51:07 +0300

    debbugging

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,27 +1,46 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+#include <limits.h>
 
 /**
-* max - Returns the maximum of two integers
-* @a: First integer
-* @b: Second integer
-* Return: The maximum of a and b
-*/
-int max(int a, int b)
-{
-return (a > b ? a : b);
-}
-
-/**
-* height - Calculate the height of a binary tree
+* binary_tree_height - Calculate the height of a binary tree
 * @tree: Pointer to the root node of the tree
 * Return: The height of the tree
 */
-int height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (!tree)
-return (-1);
+if (tree == NULL)
+return (0);
 
-return (1 + max(height(tree->left), height(tree->right)));
+size_t left_height = binary_tree_height(tree->left);
+size_t right_height = binary_tree_height(tree->right);
+
+return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+* is_avl_util - Utility function to check if a binary tree is a valid AVL tree
+* @tree: Pointer to the root node of the tree to check
+* @min: Minimum allowed value in the tree
+* @max: Maximum allowed value in the tree
+* Return: 1 if tree is a valid AVL Tree, 0 otherwise
+*/
+int is_avl_util(const binary_tree_t *tree, int min, int max)
+{
+if (tree == NULL)
+return (1);
+
+if (tree->n <= min || tree->n >= max)
+return (0);
+
+int left_height = binary_tree_height(tree->left);
+int right_height = binary_tree_height(tree->right);
+
+if (abs(left_height - right_height) > 1)
+return (0);
+
+return (is_avl_util(tree->left, min, tree->n) &&
+is_avl_util(tree->right, tree->n, max));
 }
 
 /**
@@ -31,19 +50,8 @@ return (1 + max(height(tree->left), height(tree->right)));
 */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-if (!tree)
+if (tree == NULL)
 return (0);
 
-int left_height, right_height;
-
-left_height = height(tree->left);
-right_height = height(tree->right);
-
-if (abs(left_height - right_height) > 1)
-return (0);
-
-if (!binary_tree_is_avl(tree->left) || !binary_tree_is_avl(tree->right))
-return (0);
-
-return (1);
+return (is_avl_util(tree, INT_MIN, INT_MAX));
 }
