827b4d8e35da4d99419e82107f5c151c12ae6329
jayricka <jayricka17@gmail.com>
2023-09-01 04:34:22 +0300

    advance task

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,46 +1,49 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * max - Returns the maximum of two integers
- * @a: First integer
- * @b: Second integer
- * Return: The maximum of a and b
- */
+* max - Returns the maximum of two integers
+* @a: First integer
+* @b: Second integer
+* Return: The maximum of a and b
+*/
 int max(int a, int b)
 {
-    return (a > b ? a : b);
+return (a > b ? a : b);
 }
 
 /**
- * height - Computes the height of a binary tree
- * @tree: Pointer to the root node of the tree
- * Return: The height of the tree
- */
+* height - Calculate the height of a binary tree
+* @tree: Pointer to the root node of the tree
+* Return: The height of the tree
+*/
 int height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (1 + max(height(tree->left), height(tree->right)));
+if (!tree)
+return (-1);
+
+return (1 + max(height(tree->left), height(tree->right)));
 }
 
 /**
- * binary_tree_is_avl - Checks if a binary tree is a valid AVL Tree
- * @tree: Pointer to the root node of the tree to check
- * Return: 1 if the tree is a valid AVL Tree, 0 otherwise
- */
+* binary_tree_is_avl - Checks if a binary tree is a valid AVL Tree
+* @tree: Pointer to the root node of the tree to check
+* Return: 1 if tree is a valid AVL Tree, 0 otherwise
+*/
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+if (!tree)
+return (0);
 
-    if (tree == NULL)
-        return (1);
+int left_height, right_height;
 
-    left_height = height(tree->left);
-    right_height = height(tree->right);
+left_height = height(tree->left);
+right_height = height(tree->right);
 
-    if (abs(left_height - right_height) > 1)
-        return (0);
+if (abs(left_height - right_height) > 1)
+return (0);
 
-    return (binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right));
+if (!binary_tree_is_avl(tree->left) || !binary_tree_is_avl(tree->right))
+return (0);
+
+return (1);
 }
